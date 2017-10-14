9fccc581d21ead2fad0c48ee965d50e587030e3c
fiatboy <onar651@gmail.com>
2017-10-14 18:18:32 +0200

    5.Push

--- MillProgramming/src/app/WelcomeScreenController.java
+++ MillProgramming/src/app/WelcomeScreenController.java
@@ -4,9 +4,16 @@ import javafx.fxml.FXML;
 
 public class WelcomeScreenController {
 	
+	
+	
 	@FXML
 	void hanldeButton() {
 		VistaNavigator.loadVista(VistaNavigator.VISTA_2);
+	}
+	
+	@FXML
+	void handleExit() {
+		System.exit(0);
 	}
 
 }
