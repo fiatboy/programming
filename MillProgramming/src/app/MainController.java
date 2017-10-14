a4eec198a7dd70018bfb65f5ee4eef9651b9ab8c
fiatboy <onar651@gmail.com>
2017-10-14 18:01:08 +0200

    4. Push

--- MillProgramming/src/app/MainController.java
+++ MillProgramming/src/app/MainController.java
@@ -2,9 +2,13 @@ package app;
 
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Circle;
 
 public class MainController {
+	
+	
 	
 	@FXML
 	private StackPane vistaHolder;
@@ -12,5 +16,7 @@ public class MainController {
 	public void setVista(Node node) {
 		vistaHolder.getChildren().setAll(node);
 	}
+	
+	
 
 }
--- MillProgramming/src/app/Vista1Controller.java
+++ MillProgramming/src/app/Vista1Controller.java
@@ -1,12 +1,25 @@
 package app;
 
 import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Circle;
 
 public class Vista1Controller {
 	
 	@FXML
+	Circle circle;
+	
+	MouseEvent event;
+	
+	@FXML
 	void handleNext() {
 		VistaNavigator.loadVista(VistaNavigator.VISTA_1);
+	}
+	
+	@FXML
+	private void hanldeDrag() {
+		circle.setCenterX(event.getSceneX());
+		circle.setCenterY(event.getSceneY());
 	}
 
 }
