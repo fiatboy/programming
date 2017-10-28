b033f349bf0dbc75eec4926a810a792977a70e76
fiatboy <onar651@gmail.com>
2017-10-28 12:20:58 +0200

    6. Push Bewegung des Kreises

--- MillProgramming/src/app/Vista1Controller.java
+++ MillProgramming/src/app/Vista1Controller.java
@@ -1,15 +1,23 @@
 package app;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.input.DragEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
 
 public class Vista1Controller {
 	
 	@FXML
 	Circle circle;
 	
-	MouseEvent event;
+	@FXML
+	Text target;
+	
+	double sceneX, sceneY;
+	double translateX, translateY;
+	
 	
 	@FXML
 	void handleNext() {
@@ -17,9 +25,29 @@ public class Vista1Controller {
 	}
 	
 	@FXML
-	private void hanldeDrag() {
-		circle.setCenterX(event.getSceneX());
-		circle.setCenterY(event.getSceneY());
+	private void handlePress(MouseEvent mouse) {
+		sceneX = mouse.getSceneX();
+		sceneY = mouse.getSceneY();
+		translateX = ((Circle)(mouse.getSource())).getTranslateX();
+		translateY = ((Circle)(mouse.getSource())).getTranslateY();
 	}
+	
+	@FXML
+	private void handleDrag(MouseEvent mouse) {
+		double offsetX = mouse.getSceneX() - sceneX;
+		double offsetY = mouse.getSceneY() - sceneY;
+		
+		double newTranslateX = translateX + offsetX;
+		double newTranslateY = translateY + offsetY;
+		
+		((Circle)(mouse.getSource())).setTranslateX(newTranslateX);
+		((Circle)(mouse.getSource())).setTranslateY(newTranslateY);
+	}
+	
+	@FXML
+	private void handleDragOver(DragEvent event) {
+		 
+	}
+	
 
 }
